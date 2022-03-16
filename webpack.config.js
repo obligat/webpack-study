066ea1007efd206427dc864cc1207097a6964c7a
杜金桥 <dujinqiao@dujinqiaodeMacBook-Pro.local>
2022-03-16 15:08:27 +0800

    feat: add lerna

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   mode: "production", // 如果不添加就会警告
-  entry: {
-    index: "./src/index.js", // 一个入口文件
-    chunk1: "./src/chunk1.js" // 两一个入口文件
-  },
+  entry: "./src/index.js",
   output: {
     filename: "[name].bundle.js" // 出口文件
   }
